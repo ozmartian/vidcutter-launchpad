b01e841f75a1d6e37d0db9acd24ce50bef109efe
Pete Alexandrou <pete@ozmartians.com>
2017-01-09 12:08:34 +0800

    v2.0.1

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
+
+__packager__ = 'deb'  # [ pypi | arch | deb ]
--- setup.py
+++ setup.py
@@ -10,22 +10,35 @@ from re import match
 
 from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
 
-def get_version(filename='__init__.py'):
+def get_value(varname, filename='__init__.py'):
     with open(path.join(here, filename), encoding='utf-8') as initfile:
         for line in initfile.readlines():
-            m = match('__version__ *= *[\'](.*)[\']', line)
+            m = match('__%s__ *= *[\'](.*)[\']' % varname, line)
             if m:
                 return m.group(1)
 
+
 def get_description(filename='README.md'):
     with open(path.join(here, filename), encoding='utf-8') as f:
-        return f.read()
+        file = list(f)
+    desc = ''
+    for item in file[6: len(file)]:
+        desc += item
+    return desc
+
 
 def get_architecture():
     bits = struct.calcsize('P') * 8
     return 'win-amd64' if bits == 64 else 'win32'
+
+
+def get_install_requires():
+    if packager == 'pypi':
+        return ['PyQt5 >= 5.5', 'qtawesome']
+    else:
+        return ['qtawesome']
+
 
 def get_package_data():
     if sys.platform == 'win32':
@@ -40,9 +53,24 @@ def get_package_data():
     else:
         return ['LICENSE', 'README.md']
 
+
+def get_data_files():
+    if sys.platform.startswith('linux'):
+        return [
+            ('/usr/share/pixmaps', ['data/icons/vidcutter.png']),
+            ('/usr/share/applications', ['data/desktop/vidcutter.desktop'])
+        ]
+    else:
+        return []
+
+
+here = path.abspath(path.dirname(__file__))
+
+packager = get_value('packager')
+
 setup(
     name='vidcutter',
-    version=get_version(),
+    version=get_value('version'),
     author='Pete Alexandrou',
     author_email='pete@ozmartians.com',
     description='FFmpeg based video cutter & joiner with a modern PyQt5 GUI',
@@ -56,14 +84,11 @@ setup(
 
     setup_requires=['setuptools'],
 
-    install_requires=['qtawesome'],
+    install_requires=get_install_requires(),
 
     package_data={'vidcutter': get_package_data()},
 
-    data_files=[
-        ('/usr/share/pixmaps', ['data/pixmaps/vidcutter.png']),
-        ('/usr/share/applications', ['data/desktop/vidcutter.desktop'])
-    ],
+    data_files=get_data_files(),
 
     entry_points={'gui_scripts': ['vidcutter = vidcutter.vidcutter:main']},
 
